5015a645b2ece1a9217d2588304479bbe1d94a30
Fégan <26162328+Naoroy@users.noreply.github.com>
2023-12-05 18:26:35 +0100

    WIP

--- golang/day_two/main.go
+++ golang/day_two/main.go
@@ -6,28 +6,34 @@ import (
   "strings"
 )
 
-func Answer() (string) {
+func Answer() (int) {
+  var idSum int
   input, err := os.ReadFile("./input/day_two.txt")
 
+  redCubes := 12
+  greenCubes := 13
+  blueCubes := 14
   if err != nil {
     log.Fatal(err)
   }
   games := GetGames(string(input))
 
-  log.Println(games)
-  throws := strings.Split(string(games[0]), ";")
-  log.Println(throws)
-
-  return "Day two"
+  // Iterate over games and validate each (ValidateGame)
+    // split by comma and find <int> <color> then validate against total available
+  return idSum
 }
 
-func GetGames(input string) ([]string) {
+func GetGames(input string) ([][]string) {
   lines := strings.Split(string(input), "\n")
-  log.Println(lines)
+  var throws [][]string
 
-  for i := 0; i < len(lines) - 1; i++ {
+  for i := 0; i <= len(lines) - 1; i++ {
+    if lines[i] == "" {
+      continue
+    }
     lines[i] = strings.Split(lines[i], ":")[1]
+    throws = append(throws, strings.Split(lines[i], ";"))
   }
 
-  return lines
+  return throws
 }
--- golang/main.go
+++ golang/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
   "fmt"
-  "aoc/day_one"
+  // "aoc/day_one"
   "aoc/day_two"
 )
 
 func main() {
-  fmt.Println("Day one", day_one.Answer())
+  // fmt.Println("Day one", day_one.Answer())
   fmt.Println("Day two", day_two.Answer())
 }
