cac165df9f96f5612522f32293d004fe1132184c
Fégan <26162328+Naoroy@users.noreply.github.com>
2023-12-08 16:56:06 +0100

    pseudo code

---
+++ golang/pseudo.go
@@ -0,0 +1,46 @@
+package Helene
+
+func Main() {
+/*
+Si le nombre ajdacent à "rouge" est >12 alors le jeu N est false
+Si le nombre adjacent à "vert" est >13 alors je jeu N est false
+Si le nombre ajdacent à "bleu" est >14 alors le jeu N est false
+
+est ce que l'index i est un nombre ?
+Si oui est-ce l'index i+2 est un b?
+Si oui est-ce que l'index i >14
+Si oui alors le jeu est faux
+  break
+Sinon
+
+Boucle sur chaque ligne
+Boucle sur chaque charactère de  la ligne
+Est-ce que l'index i est un nombre?
+Est-ce que ce nombre est supérieur à 14?
+Si oui alors le jeu est faux
+  break
+Sinon est ce que le nombre est supérieur à 13 & l'index i+2 = "v"?
+Si oui alors le jeu est faux
+  break
+Sinon est-ce que le nombre est supérieur à 12 & l'index i+2 = "r"?
+Si oui alors le jeu est faux
+  break
+Sinon continuer jusqu'au prochain nombre.
+Fermes les 2 boucle
+
+Mettre le contenu du game 1 dans un tableau
+  Faire une boucle sur la première ligne
+  If index i = ":" tronquer tout ce qu'il y a avant et ne garder que ce
+
+Mettre le contenu du tirage 1 dans un tableau
+  If index i = ";" alors ce qu'il y a avant est un tirage append
+  tableautirage = append 
+
+Si un tirage est faux alors le game 1 est faux
+func isgametrue()
+func istiragetrue()
+
+
+
+*/
+}
