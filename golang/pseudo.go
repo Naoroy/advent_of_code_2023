8aa2c9453955b38863ab923972d88bb166fb87c3
Fégan <26162328+Naoroy@users.noreply.github.com>
2023-12-09 09:38:07 +0100

    Finish pseudo code and start implementing

--- golang/pseudo.go
+++ golang/pseudo.go
@@ -1,6 +1,25 @@
-package Helene
+package main
 
-func Main() {
+import (
+  "fmt"
+  "os"
+)
+
+func main() {
+  readfileandconverttostring()
+  // sumofpossiblegames()
+  // isnumber()
+}
+
+func readfileandconverttostring(){
+  data, err := os.ReadFile("input/day_two.txt")
+  if err!=nil {
+    fmt.Println(err)
+  }
+  fmt.Println(string(data))
+}
+//for i = 0 ; i < 
+
 /*
 Si le nombre ajdacent à "rouge" est >12 alors le jeu N est false
 Si le nombre adjacent à "vert" est >13 alors je jeu N est false
@@ -13,7 +32,9 @@ Si oui alors le jeu est faux
   break
 Sinon
 
-Boucle sur chaque ligne
+
+
+Boucle  sur chaque ligne
 Boucle sur chaque charactère de  la ligne
 Est-ce que l'index i est un nombre?
 Est-ce que ce nombre est supérieur à 14?
@@ -26,7 +47,12 @@ Sinon est-ce que le nombre est supérieur à 12 & l'index i+2 = "r"?
 Si oui alors le jeu est faux
   break
 Sinon continuer jusqu'au prochain nombre.
-Fermes les 2 boucle
+FERMER LA Boucle
+ajouter l'index à un tableau
+FERMER la boucle
+
+
+
 
 Mettre le contenu du game 1 dans un tableau
   Faire une boucle sur la première ligne
@@ -39,8 +65,4 @@ Mettre le contenu du tirage 1 dans un tableau
 Si un tirage est faux alors le game 1 est faux
 func isgametrue()
 func istiragetrue()
-
-
-
 */
-}
